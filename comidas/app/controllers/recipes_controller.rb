124b41ddb3b30689106cf5949637303ef444187b
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-10-28 12:05:13 +1100

    Filters for meal_type's working

--- comidas/app/controllers/recipes_controller.rb
+++ comidas/app/controllers/recipes_controller.rb
@@ -1,7 +1,16 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+
+    if params[:meal_type] == "breakfast"
+      @recipes = Recipe.where(meal_type: "Breakfast")
+    elsif params[:meal_type] == "lunch"
+      @recipes = Recipe.where(meal_type: "Lunch")
+    elsif params[:meal_type] == "dinner"
+      @recipes = Recipe.where(meal_type: "Dinner")
+    else
+      @recipes = Recipe.all
+    end
   end
 
   # method to add the recipe to the users meal plan
