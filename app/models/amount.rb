b3f5045637481f23eb485c1bcd334ff02e079146
Joshua Richardson <joshrichardson1234@hotmail.com>
2015-01-13 15:54:22 +1100

    Refactoring

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: amounts
-#
-#  id            :integer          not null, primary key
-#  ingredient_id :integer
-#  recipe_id     :integer
-#  grams         :integer
-#  created_at    :datetime
-#  updated_at    :datetime
-#
-
 class Amount < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :recipe
